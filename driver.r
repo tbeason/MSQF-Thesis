f7237106822fdd7e1757a631adc3e3e4269a551d
Tyler Beason <tbeas12@gmail.com>
2015-03-16 01:25:18 -0500

    changed parameter meaning

--- driver.r
+++ driver.r
@@ -4,9 +4,11 @@
 source('~/GitHub/MSQF-Thesis/schmidt RK2.r')
 source('~/GitHub/MSQF-Thesis/kMC.r')
 source('~/GitHub/MSQF-Thesis/LuxGrid.r')
-
-model <- SchmidtRK2(alpha=0,beta=1,gamma=1,delta=1,d=0,rho=0,n0=0.4,p0=100.5,dt=0.01,N=25,pf=105)
-modelc <- CPTRK2(alpha=0,beta=1,gamma=1,delta=1,d=0,rho=0,n0=0.4,p0=100.5,dt=0.01,N=25,pf=105,Fconf=0.1,Tconf=1)
+a <- 1
+b <- 2
+z <- 1.25
+model <- SchmidtRK2(alpha=z/b,beta=b,gamma=1,delta=1,d=0,rho=0,n0=0.4,p0=10.5,dt=0.01,N=50,pf=10)
+modelc <- CPTRK2(alpha=z/b,beta=b,gamma=1,delta=1,d=0,rho=0,n0=0.4,p0=10.5,dt=0.01,N=50,pf=10,Fconf=1,Tconf=1)
 matplot(model[,1],cbind(model[,3],modelc[,3]),type="l",ylab="Price",xlab="")
 matplot(model[,1],cbind(model[,2]-0.5,modelc[,2]-0.5),type="l",ylab="Excess Demand",xlab="")
 matplot(model[,1],cbind(model[,4],modelc[,4]),type="l",ylab="Utility",xlab="")
@@ -19,6 +21,7 @@ matplot(model[,1],cbind(model[,4],modelc[,4]),type="l",ylab="Utility",xlab="")
 cpt <- CPT1(4000,6000,p0=9.8,alpha=1,beta=1,gamma=1,delta=1,numIt=1000,numTrials=10,dt=0.05,conf=1,pf=10)
 plot(cpt[[1]],type="l",main="Price")
 plot(cpt[[2]],type="l",main="Buyers")
+
 # require(reshape2)
 # require(ggplot2)
 # gr <- LuxGrid(L=100,maxit=100000,n0=0.7,p0=105,pf=100,alpha=1,beta=1,gamma=0.1)
