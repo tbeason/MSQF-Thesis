3363d4abbd58d3c2ed9222d13fb1149fd914c81e
Tyler Beason <tbeas12@gmail.com>
2015-04-24 00:53:12 -0500

    more writing support

--- RK2.r
+++ RK2.r
@@ -1,12 +1,13 @@
 ##### normal functionality as found in the Schmidt papers
-RK2.me <- function(alpha1=1,alpha2=1,beta=1,gamma=1,delta=1,nu=1,d=0,rho=0,n0=0.4,p0=1.05,dt=0.01,N=25,pf=1,Tf,r,ma=5){
-  T <- N/dt+1
-  n_plus <- numeric(T)
-  price <- numeric(T)
-  util <- numeric(T)
+RK2.me <- function(alpha=1,kappa=1,beta=1,gamma=1,nu=1,rho1=0,rho2=0,n0=0.4,p0=1.05,dt=0.01,numPeriods=25,pf=1,Tf,r,ma=5){
+  Tnum <- numPeriods/dt+1
+  n_plus <- numeric(Tnum)
+  price <- numeric(Tnum)
+  util <- numeric(Tnum)
   n_plus[1] <- n0
   price[1] <- p0
-  U <- function(n,p,t)
+  rands <- t(replicate(Tnum,snorm()))
+  U <- function(n,p,t,e1)
   {
     if(t*dt <= Tf*dt)
     {
@@ -26,36 +27,36 @@ RK2.me <- function(alpha1=1,alpha2=1,beta=1,gamma=1,delta=1,nu=1,d=0,rho=0,n0=0.
       pChange <- 0
     }
     
-    alpha1*(2*n-1) + alpha2*(pChange/p) + beta*(1/nu)*L*((pf-p)/p)
+    alpha*((2*n-1)+rho1*e1) + kappa*(pChange/p) + beta*(1/nu)*L*((pf-p)/p)
   }
   
-  fn <- function(n,p,t)
+  fn <- function(n,p,t,e1)
   {
-    nu*exp(U(n,p,t))*(1-n) - nu*exp(-1*U(n,p,t))*n
+    nu*exp(U(n,p,t,e1))*(1-n) - nu*exp(-1*U(n,p,t,e1))*n
   }
   
-  fp <- function(n)
+  fp <- function(n,e2)
   {
-    gamma*(delta*(2*n-1)+d*rnorm(1))+rho*rnorm(1)
+    gamma*((2*n-1)+rho2*e2)
   }
   
-  util[1] <- U(n0,p0,1)
-  for(i in 2:T)
+  util[1] <- U(n0,p0,1,rands[1,1])
+  for(i in 2:Tnum)
   {
-    np <- n_plus[i-1]+ 0.5*dt*fn(n_plus[i-1],price[i-1],i-1)
-    pp <- price[i-1] + 0.5*dt*fp(n_plus[i-1])
-    n_plus[i] <- n_plus[i-1] + dt*fn(np,pp,i)
-    price[i] <- price[i-1] + dt*fp(np)
-    util[i] <- U(n_plus[i],price[i],i)
+    np <- n_plus[i-1]+ 0.5*dt*fn(n_plus[i-1],price[i-1],i-1,rands[i,1])
+    pp <- price[i-1] + 0.5*dt*fp(n_plus[i-1],rands[i,2])
+    n_plus[i] <- n_plus[i-1] + dt*fn(np,pp,i,rands[i,1])
+    price[i] <- price[i-1] + dt*fp(np,rands[i,2])
+    util[i] <- U(n_plus[i],price[i],i,rands[i,1])
   }
-  t <- seq(0,N,dt)
+  t <- seq(0,numPeriods,dt)
   #as.matrix(cbind(t,n_plus,price,util))
   price
 }
 #####
 
 ##### normal functionality as found in the Schmidt papers
-RK2.orig <- function(alpha1=1,beta=1,gamma=1,delta=1,nu=1,d=0,rho=0,n0=0.4,p0=1.05,dt=0.01,N=25,pf=1){
+RK2.orig <- function(alpha=1,beta=1,gamma=1,delta=1,nu=1,d=0,rho=0,n0=0.4,p0=1.05,dt=0.01,N=25,pf=1){
   T <- N/dt+1
   n_plus <- numeric(T)
   price <- numeric(T)
@@ -64,7 +65,7 @@ RK2.orig <- function(alpha1=1,beta=1,gamma=1,delta=1,nu=1,d=0,rho=0,n0=0.4,p0=1.
   price[1] <- p0
   U <- function(n,p,t)
   {    
-    alpha1*fp(n)/p + beta*(pf-p)
+    alpha*fp(n)/p + beta*(pf-p)
   }
   
   fn <- function(n,p,t)
@@ -90,3 +91,14 @@ RK2.orig <- function(alpha1=1,beta=1,gamma=1,delta=1,nu=1,d=0,rho=0,n0=0.4,p0=1.
   as.matrix(cbind(t,n_plus,price,util))
 }
 #####
+
+
+#### Box-Muller algorithm for generating white noise random variables (standard normal)
+snorm <- function(){
+  u1 <- runif(1)
+  u2 <- runif(1)
+  z1 <- ((((-2)*log(u1))^(1/2))*cos(2*u2*pi))
+  z2 <- ((((-2)*log(u1))^(1/2))*sin(2*u2*pi))
+  z <- c(z1,z2)
+  return(z)
+}
--- driver.r
+++ driver.r
@@ -6,35 +6,45 @@ source('~/GitHub/MSQF-Thesis/kMC.r')
 
 require("PerformanceAnalytics")
 require(plyr)
+require(plot3D)
 b <- 1
-a1 <- 1
-a2 <- 1
-gamma <- 1
-delta <- 1
+a <- 1
+k <- 1
+g <- 1
 nu <- 1
 n0 <- 0.5
 dt <- 0.05
-N <- 25
+numPeriods <- 200
 pf <- 1
-Tf <- 0.75*N/dt
+Tf <- 0.75*numPeriods/dt
 r <- 0.0
 ma <- 5
 p0 <- 1.05
 
 
-Time <- seq(0,N,dt)
+Time <- seq(0,numPeriods,dt)
 
-params <- seq(2,20,1)
-Price <- t(laply(params,.fun=RK2.me,alpha1=a1,alpha2=a2,beta=b,delta=delta,gamma=gamma,nu=nu,n0=n0,d=0,rho=0,p0=p0,dt=dt,N=N,pf=pf,Tf=Tf,r=r ))
-persp3D(x=Time,y=params,z=Price,shade=0.4,ticktype="detailed",xlab="Time",zlab="Price",ylab="j",theta=40,phi=40)
+params <- rep(0.5,1000)
+Price <- t(laply(params,.fun=RK2.me,kappa=k,beta=b,gamma=g,nu=nu,n0=n0,rho1=0.05,rho2=0.05,p0=p0,dt=dt,numPeriods=numPeriods,pf=pf,Tf=Tf,r=r,ma=ma))
+#persp3D(x=Time,y=params,z=Price,shade=0.4,ticktype="detailed",xlab="Time",zlab="Price",ylab="j",theta=40,phi=40)
+rets <- apply(Price,2,FUN=function(x){Return.calculate(ts(x),method="discrete")})
+rets <- rets[-1,]
+krtos <- apply(rets,2,FUN=function(x){kurtosis(x,method="sample")})
+skew <- apply(rets,2,FUN=function(x){skewness(x,method="sample")})
+adf0r <- apply(rets,2,FUN=function(x){adf.test(x,k=0)$p.value})
+adf1r <- apply(rets,2,FUN=function(x){adf.test(x,k=1)$p.value})
+kpssTr <- apply(rets,2,FUN=function(x){kpss.test(x,null="Trend")$p.value})
+kpssLr <- apply(rets,2,FUN=function(x){kpss.test(x,null="Level")$p.value})
+shap2 <- apply(rets,2,FUN=function(x){shapiro.test(x)$p.value})
 
-matplot(Time,Price,type='l',lty=1,main="Varying beta")
+matplot(Time,Price,type='l',lty=1,main="Varying alpha")
 legend("topright",legend=params,lty=1)
 
+matplot(Time,cbind(modeld[,3],model[,3]),ylab="Price",lty=1,type="l",main="Model Comparison")
+legend("topright",legend=c("Meanvalue","Stochastic"),lty=1,col=c(1,2))
 
-
-model <- RK2.me(alpha1=a1,alpha2=a2,beta=b,gamma=gamma,delta=delta,nu=nu,d=0,rho=0,n0=n0,p0=p0,dt=dt,N=N,pf=pf,Tf=Tf,r=r,ma=ma)
-matplot(model[,1],model[,3],type="l",ylab="Price",xlab="Time",main="Price (RK2)")
+modeld <- RK2.me(alpha=a,kappa=k,beta=b,gamma=g,nu=nu,n0=n0,rho1=0,rho2=0,p0=p0,dt=dt,numPeriods=numPeriods,pf=pf,Tf=Tf,r=r,ma=ma )
+matplot(modeld[,1],modeld[,3],type="l",ylab="Price",xlab="Time",main="Price (RK2)")
 matplot(model[,1],model[,2]-0.5,type="l",ylab="Excess Demand",xlab="", main="Demand (RK2)")
 matplot(model[,1],model[,4],type="l",ylab="Utility",xlab="", main ="Utility (RK2)")
 hist(diff(log(model[,3])), main="Dist of Log Returns (RK2)",xlab="Log Return")
